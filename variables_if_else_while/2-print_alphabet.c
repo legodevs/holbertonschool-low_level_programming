96f7a776de2d9c2db37e21862ba98560b5ec9a54
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-16 13:41:00 -0500

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Always (SUccess)
+ */
+
+int main(void)
+
+{
+    char c;
+
+    for (c='a'; c <= 'z'; c++)
+    putchar(c);
+    putchar('\n');
+    return (0);
+
+}
