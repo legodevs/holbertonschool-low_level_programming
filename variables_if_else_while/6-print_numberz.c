3eb993c6399f22c8cee7abe0e6bf352c93892ce4
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-17 10:22:41 -0500

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(d);
+		putchar('\n');
+	
+	return (0);
+
+}
