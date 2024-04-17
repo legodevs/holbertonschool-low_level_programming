92cf19a977f0e18eaab5fa769c37c61bb5fd324b
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-17 17:14:03 -0500

    task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int dig, comb;
+
+	for (dig = 0; dig <= 98; dig++)
+	{
+
+	for (comb = dig + 1; comb <= 99; comb++)
+	{
+	putchar(dig / 10 );
+	putchar(dig % 10 );
+	putchar(' ');
+	putchar(comb / 10 );
+	putchar(comb % 10 );
+	if (dig == 98 && comb == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
