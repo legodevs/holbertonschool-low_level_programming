2de18c4755f329bde5837b612de7c13c1e0d19f7
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-17 15:06:59 -0500

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int dig;
+
+	for (dig = '0'; dig <= '9'; dig++)
+	{
+	putchar(dig);
+	if (dig != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
