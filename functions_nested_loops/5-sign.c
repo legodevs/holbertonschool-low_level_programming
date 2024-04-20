bc6da2cc594e2f070980652bd3d0456a471414ef
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-19 20:39:46 -0500

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print _sign - Prints the sign of a number
+ * @n: the number to get desing
+ * Return: 1 for positive number, -1 for negative number or zero for anything else
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n >0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
