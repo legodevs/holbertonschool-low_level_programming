89f8bec088728123ed814b4cf46025875b0e226c
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-19 20:54:10 -0500

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print _sign - Prints the sign of a number
  * @n: the number to get desing
- * Return: 1 for positive number, -1 for negative number or zero for anything else
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
@@ -12,16 +12,16 @@ int print_sign(int n)
 	if (n >0)
 	{
 		_putchar('+');
-	return (1);
+		return (1);
 	}
 	if (n < 0)
 	{
 		_putchar('-');
-	return (-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-	return (0);
+		return (0);
 	}
 }
