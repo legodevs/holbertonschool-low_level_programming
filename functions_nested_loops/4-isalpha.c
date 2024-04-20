1b557ff74f19fffc589e9e1ba6c1edc3a8d0b422
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-19 20:17:21 -0500

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabetic character
+ * @c: The character is checked
+ *rReturn: 1 for alphabetic character or 0 for other
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
