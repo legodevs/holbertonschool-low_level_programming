67f6d5f326ede0fa76922cd976267476ea572fea
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-27 16:39:46 -0500

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if parameter is a digit or character
+ * @c: parameter
+ * Return: 1 if its a number 0 otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	{
+	return (0);
+	}
+}
