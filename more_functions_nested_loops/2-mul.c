c073b558beb802e92a9fb9e6e86ec74b986292c2
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-27 16:59:17 -0500

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Multiplication
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
