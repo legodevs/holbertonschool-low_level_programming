5515a6dbd8ee63c4dba4d6e8da825d15bd0cd8d5
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-27 23:07:56 -0500

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: is the size of the square
+ */
+
+void print_square(int size)
+
+{
+
+	int row, cha;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (cha = 0; cha < size; cha++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
