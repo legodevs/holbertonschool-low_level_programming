e02e35d05031cc6c5395c972417af0ae6c5b3357
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-04-27 22:39:59 -0500

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the character _ should be printed
+ */
+
+void print_diagonal(int n)
+
+{
+	int und, spa;
+
+	for (und = 0; und < n ; und++)
+	{
+		if (n > 0)
+		{
+			for (spa = 0; spa < und; spa++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
