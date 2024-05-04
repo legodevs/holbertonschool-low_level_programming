f7414c46b1611b060eb92385b85f062ccef1866f
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-05-03 21:04:01 -0500

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap the values of two integers
+ *@a: first integer to be swapped
+ *@b: second integer to be swapped
+ * Return : nothing
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
