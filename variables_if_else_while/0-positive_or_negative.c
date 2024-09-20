d2659bf568054d385cadc682298d7aa831b360e5
Leonardo Gomez <leonardo.gomez.es@gmail.com>
2024-09-20 18:24:35 -0500

    C - Variables, if, else, while. task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,19 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n < 0)
-	{
-		printf("%d is %s\n", n, "negative");
-	}
-	else if (n > 0)
-	{
-		printf("%d is %s\n", n, "positive");
-	}
-	else
-	{
-		printf("%d is %s\n", n, "zero");
-	}
+		if (n < 0)
+		{
+			printf("%d is %s\n", n, "negative");
+		}
+		else if (n > 0)
+		{
+			printf("%d is %s\n", n, "positive");
+		}
+		else
+		{
+			printf("%d is %s\n", n, "zero");
+		}
 	return (0);
 }
 
